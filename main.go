9f1a113643c7be4d5e021004dab1734862ab55d2
gasweld <gasweld@gmail.com>
2022-09-30 18:38:32 +0400

    Add a telnet interface to reboot tasks

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"os/exec"
 	"strings"
@@ -33,6 +36,10 @@ func main() {
 	if err := json.Unmarshal(data, &config); err != nil {
 		log.Fatal(err)
 	}
+	conn, err := net.Listen("tcp", "localhost:1829")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	key := os.Getenv("MAILJET_KEY")
 	secret := os.Getenv("MAILJET_SECRET")
@@ -44,13 +51,54 @@ func main() {
 		log.Fatal("Missing email config parameter")
 	}
 	go sendReports(email, key, secret)
+
+	reboots := map[string]chan bool{}
 	for _, p := range config.Processes {
-		go run(p)
+		reboots[p.Name] = make(chan bool)
+		go run(p, reboots[p.Name])
 	}
-	select {}
+
+	for {
+		ln, err := conn.Accept()
+		if err != nil {
+			log.Printf("accept error: %v", err)
+			continue
+		}
+		b := bufio.NewReader(ln)
+		for {
+			line, err := b.ReadString('\n')
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("read error: %v", err)
+				break
+			}
+			line = strings.Trim(line, " \r\n")
+			parts := strings.Split(line, " ")
+			if len(parts) != 2 {
+				ln.Write([]byte("unknown syntax\n"))
+				continue
+			}
+			switch parts[0] {
+			case "reboot":
+				name := parts[1]
+				ch := reboots[name]
+				if ch == nil {
+					ln.Write([]byte("no task named " + name + "\n"))
+					break
+				}
+				ch <- true
+				ln.Write([]byte("ok\n"))
+			default:
+				ln.Write([]byte("unknown command\n"))
+			}
+		}
+		ln.Close()
+	}
 }
 
-func run(p proc) {
+func run(p proc, reboot <-chan bool) {
 	f, err := os.OpenFile(fmt.Sprintf("%s.log", p.Name), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		log.Fatal(err)
@@ -58,9 +106,7 @@ func run(p proc) {
 	defer f.Close()
 
 	args := strings.Split(p.Command, " ")
-
 	quits := 0
-
 	run := func() {
 		t := time.Now()
 		cmd := exec.Command(args[0], args[1:]...)
@@ -72,13 +118,26 @@ func run(p proc) {
 			return
 		}
 		log.Printf("started %s, pid = %v\n", p.Name, cmd.Process.Pid)
-		err = cmd.Wait()
-		report("%s quit after %v: %v", p.Name, time.Since(t), err)
+		quit := make(chan error, 1)
+		go func() {
+			quit <- cmd.Wait()
+		}()
+		select {
+		case err := <-quit:
+			report("%s quit after %v: %v", p.Name, time.Since(t), err)
+			quits++
+		case <-reboot:
+			log.Printf("got a reboot signal for %s, waiting for the process to exit", p.Name)
+			cmd.Process.Signal(os.Interrupt)
+			err = cmd.Wait()
+			if err != nil {
+				log.Printf("wait failed: %v", err)
+			}
+			quits = 0
+		}
 	}
-
 	for {
 		run()
-		quits++
 		if quits > 2 {
 			quits = 0
 			report("%s: taking a timeout, %s", p.Name, time.Hour.String())
